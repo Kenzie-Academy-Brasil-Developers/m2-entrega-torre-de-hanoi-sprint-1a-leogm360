a653700b22b670b41fbdbf8e76320778b40115ab
leogm360 <leogm360@gmail.com>
2021-11-29 20:01:09 -0300

    Fazer Setup Repositório
    Clonar repositório base;
    
    Criar estrutura básica de diretórios;
    
    Criar arquivos de HTML, CSS e Javascript iniciais para posterior
    modificação.

---
+++ assets/js/torre-hanoi.js
@@ -0,0 +1,23 @@
+//START imports
+//END imports
+
+//START global-variables
+//END global-variables
+
+//START dynamic-content-header
+//END dynamic-content-header
+
+//START dynamic-content-main
+//END dynamic-content-main
+
+//START dynamic-content-footer
+//END dynamic-content-footer
+
+//START functions-call
+//START functions-call
+
+//START event-listeners
+//END event-listeners
+
+// START exports
+// END exports
