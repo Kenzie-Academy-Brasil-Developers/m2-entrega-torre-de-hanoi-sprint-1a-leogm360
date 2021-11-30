98ad564f7c0dde2a6eb52e2095150e46947a19c7
LucasDutra <lucasbryan0217@gmail.com>
2021-11-29 22:44:23 -0300

    logica hanoi de adicionar e remover

--- assets/js/torre-hanoi.js
+++ assets/js/torre-hanoi.js
@@ -1,3 +1,4 @@
+const arrayColors = ["blue", "red", "orange", "yellow", "pink", "gray"];
 //START imports
 //END imports
 
@@ -21,3 +22,74 @@
 
 // START exports
 // END exports
+
+const main = document.querySelector("main");
+
+function startHanoi() {
+    addStack();
+    addId();
+    addCircle(3, 200);
+    stackClick();
+}
+
+function addStack() {
+    for (let i = 0; i < 3; i++) {
+        const sectionBox = document.createElement("section");
+        const stack = document.createElement("div");
+
+        sectionBox.id = `section-${i}`;
+        stack.classList.add("stack");
+
+        main.appendChild(sectionBox);
+        sectionBox.appendChild(stack);
+    }
+}
+
+function addId() {
+    const numerate = document.querySelectorAll("div.stack")
+    for (let i = 0; i < 3; i++) {
+        numerate[i].id = `select-${i}`;
+    }
+}
+
+function addCircle(quant, maxWidth) {
+    let count = maxWidth;
+    const numerateId = document.querySelector("div#select-0");
+    console.log(numerateId);
+    for (let i = 0; i < quant; i++) {
+        const circle = document.createElement("div");
+        circle.classList.add("circle");
+        numerateId.appendChild(circle);
+        circle.style.width = `${count -= 20}px`;
+        circle.style.backgroundColor = arrayColors[i];
+    }
+}
+
+function stackClick() {
+    let arr = [];
+    let arr2 = [];
+    let arr3 = [];
+    const section = document.querySelectorAll("section");
+    const numerateIdZero = document.querySelector("div#select-0");
+    const numerateIdOne = document.querySelector("div#select-1");
+    const numerateIdTwo = document.querySelector("div#select-2");
+    section[0].addEventListener('click', function (e) {
+        arr.push(numerateIdZero.lastElementChild)
+        console.log(numerateIdZero.lastElementChild)
+        arr[0].remove();
+    });
+    section[1].addEventListener("click" , function (e) {
+        arr2.push(numerateIdOne.lastElementChild)
+        numerateIdOne.appendChild(arr[0])
+        arr.pop()
+        console.log(arr)
+    });
+    section[2].addEventListener("click" , function (e) {
+        arr2.push(numerateIdTwo.lastElementChild)
+        numerateIdTwo.appendChild(arr2[0])
+        arr.pop()
+        console.log(arr)
+    });
+}
+
+startHanoi();
