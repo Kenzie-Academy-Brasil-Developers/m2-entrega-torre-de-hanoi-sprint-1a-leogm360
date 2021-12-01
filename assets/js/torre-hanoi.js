99cf95a3a6259d0d28132035e7aa3f3d5d1118f4
leogm360 <leogm360@gmail.com>
2021-12-01 08:53:38 -0300

    Incluir Área Controle
    Modificar arquivos index.html, style.css e torre-hanoi.js para comportar
    features bônus.

--- assets/js/torre-hanoi.js
+++ assets/js/torre-hanoi.js
@@ -5,7 +5,7 @@ let contador = 0;
 function startHanoi() {
     addStack();
     addId();
-    addCircle(3, 200);
+    addCircle(5, 170);
     // stackClick();
 }
 
@@ -32,7 +32,7 @@ function addId() {
 function addCircle(quant, maxWidth) {
     let count = maxWidth;
     const numerateId = document.querySelector("div#select-0");
-    console.log(numerateId);
+    
     for (let i = 0; i < quant; i++) {
         const circle = document.createElement("div");
         circle.classList.add("circle");
@@ -47,45 +47,85 @@ let arr = [];
 let verif = false
 let target = '';
 let pai = '';
+const selectedCircle = document.getElementById('control-selected')
+
 function stackClick(e) {
     const numerateIdTwo = document.getElementById("select-2");
     target = e.target
-    console.log(target)
+    const lastElement = target.closest("div.stack").lastElementChild;
+
     if (verif === false) {
         verif = true
         pai = e.target.parentNode;
-        console.log(pai);
     }
-    if (target.className === "circle") {
-        const lastElement = target.closest("div.stack").lastElementChild
-        arr.push(lastElement)
-        arr[0].remove()
+
+    if (target.className === "circle" && selectedCircle.childElementCount < 1) {
+
+        selectedCircle.appendChild(lastElement);
+
+        // arr.push(lastElement)
+        // arr[0].remove()
     }
-    switchClique(target, arr, pai)
+
+    switchClique(target)
+
     const textoVitoria = document.querySelector("#victory")
+
     if (numerateIdTwo.childNodes.length === 3) {
         textoVitoria.innerHTML = 'Voce venceu!'
     }
 }
 
 const contadorH2 = document.querySelector("#contador")
-function switchClique(e, arr, paiEl) {
-    console.log(paiEl)
-    if (e.className === "stack") {
-        if (e.children.length >= 0) {
-            arr.length > 0 ? e.appendChild(arr[0]) : false
-            if (e.children[0].clientWidth < e.lastElementChild.clientWidth) {
-                paiEl.appendChild(arr[0])
-                verif = false
-            } else {
-                e.appendChild(arr[0])
-                contador++
-                contadorH2.innerHTML = contador
-                verif = false
-            }
-            arr.pop()
+
+function switchClique(e) {
+    //Variables
+    const circleToPut = selectedCircle.firstChild;
+   
+    const stackClass = e.className;
+
+    //Check-target
+    if (stackClass === "stack"){
+        //variables
+        const stackedCircles = e.childElementCount;
+
+        //check-empty-stack
+        if(stackedCircles === 0){
+            e.appendChild(circleToPut);
+            contador++;
+            contadorH2.innerHTML = contador;
+        }
+
+        else{
+            //variables
+            const circleTopStackedWidth = e.lastElementChild.clientWidth;
+            const circleToPutWidth = circleToPut.clientWidth;
+            
+            //check-width
+            circleToPutWidth < circleTopStackedWidth ? e.appendChild(circleToPut) : null;
+
+            //counter
+            contador++;
+            contadorH2.innerHTML = contador;
         }
     }
+
+    // if (e.className === "stack") {
+    //     if (e.children.length >= 0) {
+    //         arr.length > 0 ? e.appendChild(arr  [0]) : false
+           
+    //         if (e.children[0].clientWidth < e.lastElementChild.clientWidth) {
+    //             paiEl.appendChild(arr[0])
+    //             verif = false
+    //         } else {
+    //             e.appendChild(arr[0])
+    //             contador++
+    //             contadorH2.innerHTML = contador
+    //             verif = false
+    //         }
+    //         arr.pop()
+    //     }
+    // }
 }
 
 startHanoi();
